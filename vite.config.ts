e87f4cbe124afd4adadc7cba838a1dd00efdfed8
afinmh <afinmaulana522@gmail.com>
2025-09-16 12:09:09 +0700

    push kan

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+// Jika Anda menggunakan plugin lain (seperti react, vue), biarkan saja
+// import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   build: {
+    target: 'esnext',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        chat: resolve(__dirname, 'chat.html')
+      }
+    }
+  }
+  ,
+  esbuild: {
     target: 'esnext'
   }
-});
+  // plugins: [react()], // contoh jika ada plugin
+})
